6541fba7b47c1423b586b13cd31394ce818a1038
Rafael Cortês <mrfyda@gmail.com>
2023-08-23 14:24:32 +0100

    feat: Add support for Suggested fixes :breaking: (#38)
    * feat: Add support for Suggested fixes
    
    * Update issue_test.go
    
    Co-authored-by: André Meira <6381457+afsmeira@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: André Meira <6381457+afsmeira@users.noreply.github.com>

--- issue.go
+++ issue.go
@@ -6,10 +6,11 @@ import (
 
 // Issue is the output for each issue found by the tool
 type Issue struct {
-	PatternID string `json:"patternId"`
-	File      string `json:"filename"`
-	Line      int    `json:"line"`
-	Message   string `json:"message"`
+	PatternID  string `json:"patternId"`
+	File       string `json:"filename"`
+	Line       int    `json:"line"`
+	Message    string `json:"message"`
+	Suggestion string `json:"suggestion,omitempty"`
 }
 
 // ToJSON returns the json representation of the issue
--- issue_test.go
+++ issue_test.go
@@ -2,8 +2,9 @@ package codacytool
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func testIssue() Issue {
@@ -18,6 +19,21 @@ func testIssue() Issue {
 		PatternID: patternID,
 	}
 }
+
+func testIssueWithSuggestion() Issue {
+	file := "src/foo.go"
+	line := 5
+	message := "Foo bar"
+	patternID := "foo"
+	suggestion := "foo := 42"
+	return Issue{
+		File:       file,
+		Line:       line,
+		Message:    message,
+		PatternID:  patternID,
+		Suggestion: suggestion,
+	}
+}
 func TestIssueToJSON(t *testing.T) {
 	issue := testIssue()
 
@@ -26,6 +42,15 @@ func TestIssueToJSON(t *testing.T) {
 		t.Error("Failed converting to JSON")
 	}
 	expectedJSON := fmt.Sprintf(`{"patternId":"%s","filename":"%s","line":%d,"message":"%s"}`, issue.PatternID, issue.File, issue.Line, issue.Message)
+
+	assert.Equal(t, expectedJSON, string(issueAsJSON))
+}
+
+func TestIssueWithSuggestionToJSON(t *testing.T) {
+	issue := testIssueWithSuggestion()
+
+	issueAsJSON, _ := issue.ToJSON()
+	expectedJSON := fmt.Sprintf(`{"patternId":"%s","filename":"%s","line":%d,"message":"%s","suggestion":"%s"}`, issue.PatternID, issue.File, issue.Line, issue.Message, issue.Suggestion)
 
 	assert.Equal(t, expectedJSON, string(issueAsJSON))
 }
